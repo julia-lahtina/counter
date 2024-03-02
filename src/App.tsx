54c2adcb5b5b0273368812618747be1cd558bcf9
julia-lahtina <j.lakhtin@gmail.com>
2024-03-02 19:42:09 +0100

    add new props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Counter} from './components/Counter';
+import {SetCounter} from './components/SetCounter';
 
 export type CounterType = {
     getIncreaseCounter: (counter: number) => void
@@ -8,36 +9,84 @@ export type CounterType = {
     counter: number
     minValue: number
     maxValue: number
+    maxInputValue: number
+    startValue: number
+    setMaxValue: (maxInputValue: number) => void
+    setStartValue: (startValue: number) => void
+    setValues: () => void
 }
 
 
 function App() {
 
-    const minValue = 0;
-    const maxValue = 5;
+    let minValue = 0;
+    let maxValue = 5;
 
-    const [counter, setCounter] = useState(minValue)
+    const [maxInputValue, setMaxValue] = useState<number>(maxValue)
+    const [startValue, setStartValue] = useState<number>(minValue)
+    const [counter, setCounter] = useState(startValue)
+
+
+    useEffect(() => {
+        let startValueAsString = localStorage.getItem('start value');
+        if (startValueAsString) {
+            let newStartValue = JSON.parse(startValueAsString);
+            setStartValue(newStartValue)
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('max value', JSON.stringify(maxInputValue));
+        localStorage.setItem('start value', JSON.stringify(startValue));
+    }, [maxInputValue, startValue])
+
+
+    const setValues = () => {
+        setStartValue(startValue)
+        setMaxValue(maxInputValue)
+    }
+
 
     const getIncreaseCounter = (counter: number) => {
         setCounter(counter + 1)
-    }
+        console.log(counter)
+        }
+
     const getResetCounter = () => {
-        setCounter(minValue)
+        setCounter(startValue)
     }
 
 
     return (
         <div className="App">
 
-            <div className="counter">
-                <Counter
-                    getIncreaseCounter={getIncreaseCounter}
-                    getResetCounter={getResetCounter}
-                    counter={counter}
-                    maxValue={maxValue}
-                    minValue={minValue}
-                />
-            </div>
+            <SetCounter
+                getIncreaseCounter={() => {
+                }}
+                getResetCounter={() => {
+                }}
+                counter={counter}
+                minValue={minValue}
+                maxValue={maxValue}
+                maxInputValue={maxInputValue}
+                startValue={startValue}
+                setMaxValue={setMaxValue}
+                setStartValue={setStartValue}
+                setValues={setValues}
+            />
+
+            <Counter
+                getIncreaseCounter={getIncreaseCounter}
+                getResetCounter={getResetCounter}
+                counter={counter}
+                maxValue={maxValue}
+                minValue={minValue}
+                maxInputValue={maxInputValue}
+                startValue={startValue}
+                setStartValue={setStartValue}
+                setMaxValue={setMaxValue}
+                setValues={setValues}
+            />
 
         </div>
     );
